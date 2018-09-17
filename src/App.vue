13985952a693217bfc76dfcad8be2d7244098cf9
1345649817@qq.com <1345649817@qq.com>
2018-09-17 16:21:40 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<style scoped>
+  .layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+.layout-header-bar {
+  background: #fff;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+.layout-logo-left {
+  width: 90%;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  margin: 15px auto;
+}
+.menu-icon {
+  transition: all 0.3s;
+}
+.rotate-icon {
+  transform: rotate(-90deg);
+}
+.menu-item span {
+  display: inline-block;
+  overflow: hidden;
+  width: 69px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
+  transition: width 0.2s ease 0.2s;
+}
+.menu-item i {
+  transform: translateX(0px);
+  transition: font-size 0.2s ease, transform 0.2s ease;
+  vertical-align: middle;
+  font-size: 16px;
+}
+.collapsed-menu span {
+  width: 0px;
+  transition: width 0.2s ease;
+}
+.collapsed-menu i {
+  transform: translateX(5px);
+  transition: font-size 0.2s ease 0.2s, transform 0.2s ease 0.2s;
+  vertical-align: middle;
+  font-size: 22px;
+}
+</style>
+<template>
+  <div class="layout">
+    <Layout :style="{minHeight: '100vh'}">
+      <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed" :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}" >
+        <Menu active-name="1-2" :open-names="['1']" theme="dark" width="auto" :class="menuitemClasses">
+          <Submenu name="1">
+            <template slot="title">
+              <Icon type="ios-analytics" /> 系统配置
+            </template>
+              <MenuItem name="1-1">平台分类</MenuItem>
+              <MenuItem name="1-2">管理员中心</MenuItem>
+              <MenuItem name="1-3">API管理</MenuItem>
+              <MenuItem name="1-4">导航列表</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-filing" />用户管理
+            </template>
+            <MenuItem name="2-1">会员管理</MenuItem>
+            <MenuItem name="2-2">会员体现</MenuItem>
+          </Submenu>
+          <Submenu name="3">
+            <template slot="title">
+              <Icon type="ios-cog" />商城管理
+            </template>
+            <MenuItem name="3-1">店铺管理</MenuItem>
+            <MenuItem name="3-2">Wap管理</MenuItem>
+            <MenuItem name="3-3">品牌管理</MenuItem>
+            <MenuItem name="3-4">商品管理</MenuItem>
+            <MenuItem name="3-5">订单管理</MenuItem>
+            <MenuItem name="3-6">合同管理</MenuItem>
+            <MenuItem name="3-7">审核管理</MenuItem>
+          </Submenu>
+          <Submenu name="4">
+            <template slot="title">
+              <Icon type="ios-cog" />公司官网
+            </template>
+            <MenuItem name="4-1">新闻列表</MenuItem>
+            <MenuItem name="4-2">官网图片</MenuItem>
+          </Submenu>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header :style="{padding: 0}" class="layout-header-bar">
+          <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
+        </Header>
+        <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isCollapsed: false
+    }
+  },
+  computed: {
+    rotateIcon () {
+      return ['menu-icon', this.isCollapsed ? 'rotate-icon' : '']
+    },
+    menuitemClasses () {
+      return ['menu-item', this.isCollapsed ? 'collapsed-menu' : '']
+    }
+  },
+  methods: {
+    collapsedSider () {
+      this.$refs.side1.toggleCollapse()
+    }
+  }
+}
+
+</script>
